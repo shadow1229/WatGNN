1bddf918e08265139c61de57764e9b667905dbf3
shadow1229 <shadow1229@snu.ac.kr>
2026-09-24 19:36:39 +0900

    Move non-WatGNN program files in the WatGNN repository into WatGNN_SI
    New repository location: https://github.com/shadow1229/WatGNN_SI

--- watgnn/Precalculated_data/single_protein_set/scripts/waterRMSD_cmp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for tg in $(cat ./targets_cmp) ;
-do
-    echo $tg
-    python waterRMSD.py ./ref/$tg.pdb ./WatGNN/$tg.pdb          > wkgb_log/$tg.dat
-done
-
-./summary_native.py  > ./native_summary.dat
--- watgnn/Preprint/single protein set/scripts/waterRMSD_cmp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for tg in $(cat ./targets_cmp) ;
-do
-    echo $tg
-    python waterRMSD.py ../wkgb_ref/$tg.pdb ../gnn_result_wkgb_patch_sorted/$tg.pdb          > wkgb_log/$tg.dat
-done
-
-./summary_native.py  > ./native_summary.dat
